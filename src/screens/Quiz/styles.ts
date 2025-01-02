1c47269fac685312a5e30a709770127318fd6c22
Michael Strahl <michaelstrahl.sm@gmail.com>
2025-01-02 13:51:38 -0300

    add progress bar after question scroll

--- src/screens/Quiz/styles.ts
+++ src/screens/Quiz/styles.ts
@@ -6,11 +6,22 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: THEME.COLORS.GREY_800,
   },
+  header: {
+    width: '100%',
+    marginBottom: 21
+  },
   question: {
     alignItems: 'center',
     paddingTop: 80,
     paddingBottom: 300,
     padding: 32,
+  },
+  title: {
+    fontSize: 16,
+    marginBottom: 7,
+    textAlign: 'center',
+    fontFamily: THEME.FONTS.BOLD,
+    color: THEME.COLORS.GREY_100,
   },
   footer: {
     flexDirection: 'row',
